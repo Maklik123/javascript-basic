93e311689d92f4b8cd6cfc1c1db021db703f0316
Maklik123 <umairmalikavan@gmail.com>
2018-11-30 15:34:01 +0500

    third challenge done

---
+++ Basics of js/Jschallenge/thirdChallenge.js
@@ -0,0 +1,45 @@
+var bills = [ 124, 48, 268 ];
+var tip = []
+var finalBill =[];
+
+function getPushArray(tips, billFinal){
+    tip.push(tips);
+    finalBill.push(billFinal);
+    
+}
+
+
+function toTip(bill){
+    if(bill < 50){
+//        var t = (bill * 20)/100;
+        var t = bill *(20/100);
+//        console.log(t);
+//        tip.push(t);
+        var f = t + bill
+        getPushArray(t,f);
+//        finalBill.push(f)
+    }
+    if(bill >=50 && bill < 200){
+        var t = bill* (15/100);
+        var f = t + bill
+        getPushArray(t,f);
+//        console.log(t)
+//        tip.push(t);
+    }
+    if(bill >200){
+        var t= bill *(10/100);
+        var f = t + bill
+        getPushArray(t,f);
+//        console.log(t);
+//        tip.push(t);
+    }
+}
+//console.log(tip);
+
+toTip(124);
+toTip(48);
+toTip(268);
+//toTip(90);
+
+console.log("Tips  "+tip)
+    console.log("FinalBill  "+finalBill);
---
+++ Basics of js/js/function.js
@@ -0,0 +1,23 @@
+function calculateAge(birthYear){
+    return 2018 - birthYear;
+}
+
+//console.log(calculateAge(1996))
+var result  = calculateAge(1996)
+
+console.log(result)
+
+function retireyear(birth, firstName){
+    var age = calculateAge(birth);
+    var retirment = 70 - age;
+    
+//    console.log(retirment, firstName);
+    if(retirment > age){
+        console.log()
+    }
+}
+
+
+retireyear(1996, 'umair ')
+retireyear(1947, 'sumair ')
+retireyear(1964, 'umar ')
